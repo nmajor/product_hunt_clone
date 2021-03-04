8c4517109e28a432fbba6eec0877950558302f78
Nicholas Major <nick@nmajor.com>
2021-03-04 21:42:12 +0000

    adding files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :examples
   get 'products/index'
   devise_for :users
   root to: 'products#index'
+  resources :products, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
